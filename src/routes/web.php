43fc41a7df1bbd6d54605d4ee5eda9c3fddae714
yudai ishige <daidai.yudai827@gmail.com>
2025-09-24 17:32:08 +0900

    変更

--- src/routes/web.php
+++ src/routes/web.php
@@ -14,4 +14,7 @@ use App\Http\Controllers\ContactController;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::get('/', [ContactController::class, 'index']);
